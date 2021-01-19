8fb7d865720d9abe633ed8a49f4dad8eea633b8c
yakir <yakirhuri21@gmail.com>
2021-01-19 19:25:46 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,166 +1,178 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <chrono> 
 
-using namespace cv;
+#include <chrono> 
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <iostream>
+
 using namespace std;
+using namespace cv;
 using namespace std::chrono; 
 
 
 
-bool DetectSpots(const cv::Scalar color, const Mat& img,
-     double threshold, double maxAreaSpot){
-    
-    Mat binaryImg(img.rows, img.cols, CV_8U, Scalar(0));
-
-    std::vector<cv::Point> points;
-    int refL = color[0]; int refA =  color[1]; int RefB =  color[2];
-    cv::Mat labImg = img.clone();
-    cvtColor(img,labImg,CV_RGB2Lab);
-    for(int i=0; i<labImg.rows; i++){
-        for(int j=0; j<labImg.cols; j++) {
-            int l = labImg.at<cv::Vec3b>(i,j)[0];
-            int a = labImg.at<cv::Vec3b>(i,j)[1];
-            int b = labImg.at<cv::Vec3b>(i,j)[2];          
-            double distnace = std::sqrt(pow(refL -l,2)  + pow(refA -a,2)
-                  + pow(RefB -b,2) );
-
-            if (distnace < threshold){
-                circle(binaryImg, cv::Point(j,i),1, Scalar(255), -1, 8, 0);                
-            }
-           
-        }
-    }
-    // imshow("binaryImg",binaryImg);
-    // imshow("labImg",labImg);
-    //         waitKey(1);
-  
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-
-    findContours(binaryImg, contours, hierarchy, CV_RETR_EXTERNAL,
-                    CV_CHAIN_APPROX_NONE, Point(0, 0));
-    
-    if (contours.size() == 1){
-        double area = contourArea(contours[0]);
-        if ( area < maxAreaSpot ){
-            // drawContours(labImg, contours,0, Scalar(0,0,0),-1, 8, hierarchy, 0, Point());
-
-            // imshow("labImg",labImg);
-            // waitKey(1);
-            return true;
-        }
-    
-        
-    }
-
-    return false;     
-
-
-}
-
 enum DRINK_TYPE { BOTTLE, CAN, BOTTLE_BEER_1_5};
 
-enum BOX_STATE { SEE_SPOT, SEE_BOTTLE, COUNT_BOTTLE_OR_CAN, PUSH_DRINK };
+enum BOX_STATE { IDLE, SEE_BOTTLE, COUNT_BOTTLE_OR_CAN, PUSH_DRINK };
 
-int main(int argc, char** argv)
-{
-    VideoCapture capture(3);
-    Mat frame;
 
-    if( !capture.isOpened() )
-        throw "Error when reading video";
+std::vector<Rect> detectBottle(const Mat& frame, cv::Rect area, 
+    CascadeClassifier bottles_cascade ) {
 
-    BOX_STATE state = SEE_SPOT;
-    auto start = high_resolution_clock::now();
-    DRINK_TYPE currentType;
+    Mat frame_gray;
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    //-- Detect faces
+    std::vector<Rect> bottles;
+    std::vector<Rect> newREctsBottles;
+
+    bottles_cascade.detectMultiScale( frame_gray, bottles, 1.1 , 3, 0,  cv::Size(50,100),  cv::Size(100,300) );
+    
+    for ( size_t i = 0; i < bottles.size(); i++ ) {
+
+        if( bottles[i].x >= area.x &&  bottles[i].x + bottles[i].width <=  area.x + area.width
+            &&  bottles[i].y >= area.y &&  bottles[i].y + bottles[i].height <=  area.y + area.height){            
+            
+            newREctsBottles.push_back(bottles[i]);
+        }
+    }
+
+    return newREctsBottles;      
+    
+}
+
+void drawDetecetdRects(Mat frame, vector<cv::Rect> rects){
+
+    for(int i =0; i < rects.size(); i++){
+        cv::rectangle( frame, rects[i], Scalar(0,255,0),2 );
+    }
+}
+
+
+int main(int argc, char** argv) {   
+    
+    CascadeClassifier bottles_cascade;
+    VideoCapture capture(0);    
+
+    if( !capture.isOpened() ){
+        cout<<"Error when reading video"<<endl;
+        return -1;
+    }
+    
     int secondThreshold = 5;
-    int secondPushing = 7;
-    cv::Scalar color( 218, 99 , 182);
+    BOX_STATE state = IDLE;
+    auto start = high_resolution_clock::now();
+  
     bool stillPushing = false;
 
-    for( ; ; )
-    {
-        capture >> frame;
+     //-- 1. Load the cascades
+    if( !bottles_cascade.load("/home/yakir/bottle/images/negative/output/cascade.xml"  )){
+        cout << "--(!)Error loading face cascade\n";
+        return -1;
+    };
+    
+     cv::Rect area(200,0,150,400);
+   
+    
+    Mat frame;
 
+    while(true) {   
+       
+        capture >> frame;
         switch (state)
         {
-            case SEE_SPOT:
-            {               
-                if( DetectSpots(color,frame, 25, 150) ){
-                    putText(frame,"SPOT", cv::Point(100,100),
+            case IDLE:
+            {   
+               
+                vector<cv::Rect> b_rects = detectBottle( frame, area, bottles_cascade);  
+                cv::rectangle( frame, area, Scalar(0,0,0),2 );
+          
+                if( b_rects.size() == 0 ){
+                    putText(frame,"IDLE", cv::Point(100,100),
                         1,3,Scalar(0,0,255),3);
-                    state = SEE_SPOT;
+                    state = IDLE;
                 } else {
                     state = SEE_BOTTLE;
+                    putText(frame,"SEE_BOTTLE", cv::Point(100,100),
+                        1,3,Scalar(0,0,255),3);
                     start = high_resolution_clock::now();
                     break;
                 }                
             }
             case SEE_BOTTLE:
             {   
-                /// if doesnt recognize the sport 
-                if( !DetectSpots(color,frame, 25, 150) ){
+                vector<cv::Rect> b_rects = detectBottle( frame, area, bottles_cascade);  
+                cv::rectangle( frame, area, Scalar(0,0,0),2 );
+                if( b_rects.size() > 0 ){
+                    
+                    drawDetecetdRects(frame,b_rects);
                     auto end = high_resolution_clock::now(); 
                     auto duration = duration_cast<seconds>(end - start); 
                     // if we pass more then X seconds, movnig to detect bottle
                     if ( duration.count() > secondThreshold){
                         putText(frame,"ALERT", cv::Point(100,100),1,3,Scalar(0,0,255),3);
-                        state = COUNT_BOTTLE_OR_CAN;
-                        break;
-                        // imshow("alert",frame);
-                        // waitKey(0);
+                        state = COUNT_BOTTLE_OR_CAN;                       
+                        imshow("COUNT_BOTTLE_OR_CAN",frame);
+                        waitKey(0);
+                        return -1;
+                         break;
                     } // we wait for the time will pass 
-                    else {
+                    else
+                    {
+                        putText(frame,"SEE_BOTTLE:" + to_string(duration.count()), cv::Point(100,100),
+                            1,3,Scalar(0,0,255),3);
                         state = SEE_BOTTLE;
                         break;
                     }                   
-                } // we saw the spot ( it was noise) 
-                else {
-                    state = SEE_SPOT;
+                } // we saw nothing
+                else 
+                {   
+                    putText(frame,"IDLE", cv::Point(100,100),
+                        1,3,Scalar(0,0,255),3);
+                    state = IDLE;
                     break;
                 }               
             }
 
-            case COUNT_BOTTLE_OR_CAN:
-            {
-                currentType = detectType(frame);
-                countMoney(currentType);
-                state = PUSH_DRINK;
-                start = high_resolution_clock::now();
-                break;
-            }
-            case PUSH_DRINK:
-            {   
-                if (!stillPushing){
-                    pushDrinkdDirection(currentType); //thread
-                    stillPushing = true;
-                }
-                auto end = high_resolution_clock::now(); 
-                auto duration = duration_cast<seconds>(end - start); 
-                if ( duration.count() > secondPushing){
+            // case COUNT_BOTTLE_OR_CAN:
+            // {
+            //     currentType = detectType(frame);
+            //     countMoney(currentType);
+            //     state = PUSH_DRINK;
+            //     start = high_resolution_clock::now();
+            //     break;
+            // }
+            // case PUSH_DRINK:
+            // {   
+            //     if (!stillPushing){
+            //         pushDrinkdDirection(currentType); //thread
+            //         stillPushing = true;
+            //     }
+            //     auto end = high_resolution_clock::now(); 
+            //     auto duration = duration_cast<seconds>(end - start); 
+            //     if ( duration.count() > secondPushing){
                     
-                    stillPushing = false;
-                    state = SEE_SPOT;
+            //         stillPushing = false;
+            //         state = IDLE;
                     
-                    break;                   
-                } else {
-                    state = PUSH_DRINK;
-                }
-                break;
-            }
+            //         break;                   
+            //     } else {
+            //         state = PUSH_DRINK;
+            //     }
+            //     break;
+            // }
             
             default:
                 break;
-        }
-        
-       
+        }   
+
         imshow("frame",frame);
-        waitKey(1);
+        waitKey(1); 
        
     }
    
    return 0;
 }
+
+
+
