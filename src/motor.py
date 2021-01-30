6b8a361d71e9fc7df120a84e2520a2d4acf0825d
yakirhuri21@gmail.com <yakirhuri21@gmail.com>
2021-01-30 20:31:58 +0200

    added motors

---
+++ src/motor.py
@@ -0,0 +1,23 @@
+# Import libraries
+import RPi.GPIO as GPIO
+import time
+
+# Set GPIO numbering mode
+GPIO.setmode(GPIO.BOARD)
+
+# Set pin 11 as an output, and set servo1 as pin 11 as PWM
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
+
+#start PWM running, but with value of 0 (pulse off)
+servo1.start(0)
+while 1:
+    for dc in range(0, 101, 5):
+        servo1.ChangeDutyCycle(dc)
+        time.sleep(0.1)
+   
+servo1.stop()
+
+GPIO.cleanup()
+print ("Goodbye")
+
